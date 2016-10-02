adcf5b3abf14a36e106aed85cf459742ebb9fba3
rshift <utvk@ya.ru>
2016-10-02 05:28:21 +0300

    hand =)

--- src/catch-it.ts
+++ src/catch-it.ts
@@ -94,9 +94,32 @@ class FpsCounter extends Label {
     }
 }
 
+class Hand extends EngineObject {
+
+    private image: HTMLImageElement;
+    x: number = 0;
+    private readonly y: number = 400;
+
+    constructor() {
+        super();
+        this.image = document.getElementById("hand") as HTMLImageElement;
+    }
+
+    public render(engine: Engine, ctx: CanvasRenderingContext2D): void {
+        let actualX = this.x - this.image.width / 2;
+
+        ctx.drawImage(this.image, actualX, this.y);
+
+        let offset = actualX + this.image.width, width = engine.canvas.width - offset;
+        if (width > 0) {
+            ctx.drawImage(this.image, this.image.width - 1, 0, 1, this.image.height, offset, this.y, width, this.image.height);
+        }
+    }
+}
+
 class Engine {
 
-    private canvas: HTMLCanvasElement;
+    public canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
     private prevTime: number = 0;
     private root: EngineObject;
@@ -139,8 +162,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let engine: Engine = new Engine(document.getElementById("catch-it") as HTMLCanvasElement);
 
     let root = new Scene();
-    root.add(new TestArrow()).
+    let hand = new Hand();
+    root.add(hand).
         add(new FpsCounter().position(10, 30).font("20px Arial"));
+
+    document.addEventListener("mousemove", (event) => {
+        hand.x = event.pageX - engine.canvas.offsetLeft;
+    });
+
 
     engine.handler(root).start();
 
